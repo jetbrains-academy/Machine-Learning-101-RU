5e95f9672aa8336633fe578ad7bf6e35c3b99500
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2019-08-20 17:10:52 +0300

    finalized test_train_split tests and implementation

--- Neighbors and Wine/Precision Recall/task.py
+++ Neighbors and Wine/Precision Recall/task.py
@@ -2,8 +2,7 @@ import numpy as np
 
 
 def train_test_split(X, y, ratio=0.8):
-    indices = np.arange(X.shape[0])
-    np.random.shuffle(indices)
+    indices = np.random.permutation(X.shape[0])
     train_len = int(X.shape[0] * ratio)
     return X[indices[:train_len]], y[indices[:train_len]], X[indices[train_len:]], y[indices[train_len:]]
 
