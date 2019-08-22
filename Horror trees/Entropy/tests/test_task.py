c91768c58ea8765766b21a35851173a9330d8054
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2019-08-22 20:08:43 +0300

    tests for the entropy

--- Horror trees/Entropy/tests/test_task.py
+++ Horror trees/Entropy/tests/test_task.py
@@ -1,6 +1,20 @@
 import unittest
+import numpy as np
+from ..task import entropy
 
 
 class TestCase(unittest.TestCase):
-    def test(self):
-        self.assertEqual(1, 0, msg="Implement tests")
+    def test_equally_divided(self):
+        a = np.array([1, 2, 1, 2, 1, 2])
+        ent = entropy(a)
+        self.assertEqual(1, ent, "Entropy for the equally divided array should be 1")
+
+    def test_homogeneous(self):
+        a = np.array([1, 1, 1, 1, 1, 1])
+        ent = entropy(a)
+        self.assertEqual(0, ent, "Entropy for the homogeneous array should be 0")
+
+    def test_random(self):
+        a = np.array([1, 2, 1, 1, 1, 1])
+        ent = entropy(a)
+        self.assertEqual(0.65, round(ent, 2), "Wrong entropy computed")
