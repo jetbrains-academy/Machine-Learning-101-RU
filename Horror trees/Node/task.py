e8dd2f217148423aae8faf22a3402d8bf47c4b5b
Ekaterina Tuzova <ekaterina.tuzova@jetbrains.com>
2019-08-22 18:57:11 +0300

    node task tests

--- Horror trees/Node/task.py
+++ Horror trees/Node/task.py
@@ -1,6 +1,6 @@
 class Node:
-    def __init__(self, column=-1, value=None, true_branch=None, false_branch=None):
-        self.column = column
-        self.value = value
+    def __init__(self, predicate_index=-1, predicate_value=None, true_branch=None, false_branch=None):
+        self.column = predicate_index
+        self.value = predicate_value
         self.true_branch = true_branch
         self.false_branch = false_branch
--- Horror trees/Node/tests/test_task.py
+++ Horror trees/Node/tests/test_task.py
@@ -1,6 +1,16 @@
 import unittest
+from ..task import Node
 
 
 class TestCase(unittest.TestCase):
-    def test(self):
-        self.assertEqual(1, 0, msg="Implement tests")
+    def test_true_branch(self):
+        node = Node(1, 2, [1,2], [3,4])
+        self.assertTrue(hasattr(node, "true_branch"), "Store true_branch in the true_branch field")
+
+    def test_false_branch(self):
+        node = Node(1, 2, [1,2], [3,4])
+        self.assertTrue(hasattr(node, "false_branch"), "Store false_branch in the true_branch field")
+
+    def test_fields(self):
+        node = Node(1, 2, [1,2], [3,4])
+        self.assertEqual(4, len(node.__dict__), "You should store all values passed to the Node object as field")
