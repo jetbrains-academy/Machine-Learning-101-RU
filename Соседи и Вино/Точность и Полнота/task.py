fix(knn - precision and recall task.py): The formula for the false negative count changed
The formula for the false negative count in precission and recall fixed,
as it used to include true positive cases

Соседи и Вино/Точность и Полнота/task.py
@@ -29,7 +29,7 @@ def precision_recall(y_pred, y_test):
     for c in np.unique(y_test):
         tp = len([i for i in range(len(y_pred)) if y_pred[i] == c and y_test[i] == c])
         fp = len([i for i in range(len(y_pred)) if y_pred[i] == c and y_test[i] != c])
-        fn = len([i for i in range(len(y_pred)) if y_pred[i] != y_test[i] and y_pred[i] != c])
+        fn = len([i for i in range(len(y_test)) if y_pred[i] != c and y_test[i] == c])
         precision = tp / (tp + fp) if tp + fp > 0 else 0.
         recall = tp / (tp + fn) if tp + fn > 0 else 0.
         class_precision_recall.append((c, precision, recall))