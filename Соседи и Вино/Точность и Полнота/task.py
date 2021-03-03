c6d24c78bd31722f67441486b667a5a8997365de
Igor <igor.gerasimov@jetbrains.com>
2021-03-03 11:39:43 +0300

    feat(knn lesson - precision and recall): The task is adjusted to not have placeholders
    - The precision_recall description slightly changed in the task.md
    - the
    precision_recall and print_precision_recall functions
    are extracted in a
    separate file
    - the function is supplied with comments to help its
    completion
    - The tests are adjusted
    
    Closes EDC-337

--- Соседи и Вино/Точность и Полнота/task.py
+++ Соседи и Вино/Точность и Полнота/task.py
@@ -1,4 +1,6 @@
 import numpy as np
+from distances import euclidean_dist, taxicab_dist
+from metrics import precision_recall, print_precision_recall
 
 
 def train_test_split(X, y, ratio=0.8):
@@ -16,34 +18,8 @@ def knn(X_train, y_train, X_test, k, dist):
     return [classify_single(x) for x in X_test]
 
 
-def euclidean_dist(x, y):
-    return np.linalg.norm(x - y)
-
-
-def taxicab_dist(x, y):
-    return np.abs(x - y).sum()
-
-
-def precision_recall(y_pred, y_test):
-    class_precision_recall = []
-    for c in np.unique(y_test):
-        tp = len([i for i in range(len(y_pred)) if y_pred[i] == c and y_test[i] == c])
-        fp = len([i for i in range(len(y_pred)) if y_pred[i] == c and y_test[i] != c])
-        fn = len([i for i in range(len(y_test)) if y_pred[i] != c and y_test[i] == c])
-        precision = tp / (tp + fp) if tp + fp > 0 else 0.
-        recall = tp / (tp + fn) if tp + fn > 0 else 0.
-        class_precision_recall.append((c, precision, recall))
-    return class_precision_recall
-
-
-def print_precision_recall(result):
-    for c, precision, recall in result:
-        print("class:", c, "\nprecision:", precision, "\nrecall:", recall, "\n")
-
-
 if __name__ == '__main__':
     wines = np.genfromtxt('wine.csv', delimiter=',')
-
     X, y = wines[:, 1:], np.array(wines[:, 0], dtype=np.int32)
     X_train, y_train, X_test, y_test = train_test_split(X, y, 0.6)
     y_predicted = knn(X_train, y_train, X_test, 5, euclidean_dist)
