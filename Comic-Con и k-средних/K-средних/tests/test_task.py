bb58f13df34d6df9fb4a12d236bef940a0a4c23d
Igor <igor.gerasimov@jetbrains.com>
2021-03-05 13:32:03 +0300

    feat(Comic-Con - kmeans): The task is transformed to exclude dependencies and placeholders
    - Comments in kmeans provided
    - Placeholders removed
    - Files for
    the
    future tasks include and hidden
    - The kmeans and init_clusters are
    extracted to a separate file clustering.py
    - The euclidean_dist
    extracted to a separate file
    - Test imports modified
    
    Closes EDC-337

--- Comic-Con и k-средних/K-средних/tests/test_task.py
+++ Comic-Con и k-средних/K-средних/tests/test_task.py
@@ -2,8 +2,9 @@ import unittest
 import numpy as np
 from numpy.testing import assert_array_equal
 
-from task import k_means, euclidean_distance
-import task
+from clustering import k_means
+from distances import euclidean_distance
+import clustering
 
 
 class TestCase(unittest.TestCase):
@@ -18,7 +19,7 @@ class TestCase(unittest.TestCase):
         expected_labels = [0, 0, 1, 1]
         expected_centers = np.array([[0, 0], [0, 1]])
 
-        task.init_clusters = lambda x, y: np.array([[0, 0], [1, 1]])
+        clustering.init_clusters = lambda x, y: np.array([[0, 0], [1, 1]])
 
         classification, clusters = k_means(X, 2, euclidean_distance)
         assert_array_equal(classification, expected_labels)
