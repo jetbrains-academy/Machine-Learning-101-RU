40460b38712c1a861fa9b0a02579b1c0111805f9
Igor <igor.gerasimov@jetbrains.com>
2021-03-18 16:41:00 +0300

    fix(Loocv and sample split): Text fixed after proofreading
    Minor text fixes
    
    Closes EDC-337

--- Соседи и Вино/Разбиение выборки/crossvalidation.py
+++ Соседи и Вино/Разбиение выборки/crossvalidation.py
@@ -4,24 +4,24 @@ from metric_classification import knn
 
 def loocv(X_train, y_train, dist):
     def loo(k):
-        # This is the counter keeping track of wrongly determined classes
+        # This is a counter keeping track of the wrongly determined classes
         c = 0
         for i in range(len(X_train)):
             # Here we should choose the current training sample of objects
             # including all but the one with index i. NumPy.vstack() could
-            # be useful to concatenate arrays with a shape of (1, N)
+            # be useful to concatenate arrays of the shape (1, N)
             x_train_cur = #TODO
             # Here we should choose the current training sample of classes
             # including all but the one with index i. NumPy.concatenate could
             # do it for a 1-d array
             y_train_cur = #TODO
-            # Here we should place a condition to check if the algorithm trained on the training samples
-            # was wrong in determining the class of the testing case.
+            # Here we should place a condition when the algorithm trained on the samples
+            # would to be wrong in determining the class of the testing case.
             # Note that the testing case is the one with index i in both samples.
             if #TODO:
                 c += 1
         return c
-    # Here we construct the list of all error counts mapped to the corresponding
+    # Here we construct a list of all error counts mapped to the corresponding
     # k values. We select the lowest one and return its corresponding k. It is shifted
     # by 1, as the array indices start with 0.
     loos = list(map(loo, range(1, len(X_train) - 1)))
--- Соседи и Вино/Разбиение выборки/task.py
+++ Соседи и Вино/Разбиение выборки/task.py
@@ -24,12 +24,12 @@ if __name__ == '__main__':
     wines = np.genfromtxt('wine.csv', delimiter=',')
     # Here the data is split into objects and classes
     # to process them separately. Note that an object and
-    # a corresponding class will have the same index
+    # the corresponding class will have the same index
     X, y = wines[:, 1:], np.array(wines[:, 0], dtype=np.int32)
     # Here we call our function to look at the result
     X_train, y_train, X_test, y_test = train_test_split(X, y, 0.6)
-    # It is convenient to add a visualization (console output at least)
-    # for each of the functions you add in a new step. Like this!
+    # It is convenient to add visualization (a console output at least)
+    # for each of the functions you add in a new step. Like this:
     print("X_train: ", "\n")
     print(X_train)
     print("y_train: ")
