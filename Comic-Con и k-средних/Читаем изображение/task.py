c24d2a640001e89e44a784e3e945fa5f64db8b9e
Igor <igor.gerasimov@jetbrains.com>
2021-03-19 12:27:41 +0300

    fix(Comic-Con - task.py): comments fixed
    - Comments
    fixed according to the
    review
    
    Closes EDC-337

--- Comic-Con и k-средних/Читаем изображение/task.py
+++ Comic-Con и k-средних/Читаем изображение/task.py
@@ -5,12 +5,12 @@ import numpy as np
 def read_image(path='superman-batman.png'):
     # Here we need to read the image using the PIL function open
     image = #TODO
-    # We reshape the image array returned into one with (M x N, 3)
+    # We reshape the image array into one with (M x N, 3)
     # shape
     return np.array(image).reshape(-1, 3)
 
 
 if __name__ == '__main__':
     image = read_image()
-    # Take a look at what does the image look like in a form of array
+    # Take a look at what the image looks like in the form of an array
     print(image)
