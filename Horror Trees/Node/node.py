bc78f18de837bec14eb150d321ddaa2ed5ae5714
sofiiako <19rubisco93@gmail.com>
2021-04-06 19:13:10 +0300

    Updated task text and code (added comments)

--- Horror Trees/Node/node.py
+++ Horror Trees/Node/node.py
@@ -1,8 +1,9 @@
 class Node:
     def __init__(self, column=-1, value=None, true_branch=None, false_branch=None):
-        #TODO
+        #TODO: implement four attributes of the class Node
         pass
 
+    # Below we defined a __repr__ method to ensure readable printing of Node instances
     def __repr__(self):
         return f'column: {self.column};\n' \
                f'value: {self.value};\n' \
