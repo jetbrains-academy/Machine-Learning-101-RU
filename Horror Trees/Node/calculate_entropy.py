cf023035b0a9fc2c47b3d0327d07499e070274d5
sofiiako <19rubisco93@gmail.com>
2021-04-06 19:38:54 +0300

    Updated Entropy task and code

--- Horror Trees/Node/calculate_entropy.py
+++ Horror Trees/Node/calculate_entropy.py
@@ -2,5 +2,9 @@ import numpy as np
 
 
 def entropy(y):
-    # TODO
-    pass
+    # use numpy.unique to obtain sorted unique elements and their counts
+    _, counts = # TODO
+    # calculate proportion of each class in the whole dataset
+    p = # TODO
+    # calculate and return entropy using the formula from the task
+    return # TODO
