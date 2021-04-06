ef0df6d653025f91476faaf0d1c63cca3f3e7883
sofiiako <19rubisco93@gmail.com>
2021-04-06 20:10:27 +0300

    Updated Predicate task and code

--- Horror Trees/Node/divide.py
+++ Horror Trees/Node/divide.py
@@ -6,9 +6,24 @@ class Predicate:
         self.value = value
 
     def divide(self, X, y):
-        # TODO
-        pass
+        # check if the value is numeric and create a boolean filter array
+        # based on the "greater or equal to" condition
+        if # TODO :
+            mask = # TODO
+        # if the value is not numeric, create the array based on the
+        # "equal to" condition
+        else:
+            mask = # TODO
+        # return the results in the following order: X1, y1, X2, y2
+        return # TODO
 
     def information_gain(self, X, y):
-        # TODO
         pass
+        # use the divide method to split the sample
+        # X1, y1, X2, y2 = # TODO
+        # calculate the fraction of X1 in the whole dataset
+        # p = # TODO
+        # use entropy function you wrote earlier and the formula
+        # from the task to calculate information gain
+        # gain = # TODO
+        # return gain
