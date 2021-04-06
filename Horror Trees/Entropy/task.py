ce72aa8bdfae1e67a5bcec15ca1c102cd0226a01
sofiiako <19rubisco93@gmail.com>
2021-04-06 15:06:36 +0300

    changed task.py for 1 and 2 sections

--- Horror Trees/Entropy/task.py
+++ Horror Trees/Entropy/task.py
@@ -1,15 +1,17 @@
-import numpy as np
+from calculate_entropy import entropy
+import pandas as pd
+from node import Node
+
+def read_data(path):
+    data = pd.read_csv(path)
+    y = data[['type']]
+    X = data.drop('type', 1)
+    return X.to_numpy(), y, X.columns.values
 
 
-def entropy(y):
-    _, counts = np.unique(y, return_counts=True)
-    p = counts / len(y)
-    return -(p * np.log2(p)).sum()
+if __name__ == '__main__':
+    node = Node(1, 2, [1, 2], [3, 4])
+    print(node)
 
-
-class Node:
-    def __init__(self, column=-1, value=None, true_branch=None, false_branch=None):
-        self.column = column
-        self.value = value
-        self.true_branch = true_branch
-        self.false_branch = false_branch
+    X, y, columns = read_data("halloween.csv")
+    print('dataset entropy:', entropy(y))
--- Horror Trees/Node/task.py
+++ Horror Trees/Node/task.py
@@ -1,19 +1,9 @@
-from calculate_entropy import entropy
-import pandas as pd
 from node import Node
-
-def read_data(path):
-    data = pd.read_csv(path)
-    y = data[['type']]
-    X = data.drop('type', 1)
-    return X.to_numpy(), y, X.columns.values
 
 
 if __name__ == '__main__':
     node = Node(1, 2, [1, 2], [3, 4])
     print(node)
 
-    X, y, columns = read_data("halloween.csv")
-    print('dataset entropy:', entropy(y))
 
 
