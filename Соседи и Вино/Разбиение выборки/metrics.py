a57a9ecbd8000a037275dbae98815611c4b021ac
mikhail <mikhail.oshukov@jetbrains.com>
2021-04-30 14:10:19 +0300

    Merge pull request #33 from jetbrains-academy/sofia/horror_trees
    Sofia/horror trees

--- Соседи и Вино/Разбиение выборки/metrics.py
+++ Соседи и Вино/Разбиение выборки/metrics.py
@@ -3,24 +3,24 @@ import numpy as np
 
 def precision_recall(y_pred, y_test):
     class_precision_recall = []
-    # Here we calculate precision and recall for each of the unique classes of the
-    # testing sample
+    # Here we calculate the precision and recall for each of the unique classes of the
+    # testing sample.
     for c in np.unique(y_test):
-        # Here we evaluate tp for the class
+        # Here we evaluate the number of tp for the class.
         tp = #TODO
-        # Here we evaluate fp for the class
+        # Here we evaluate the number of fp for the class.
         fp = #TODO
-        # Here we evaluate fn for the class
+        # Here we evaluate the number of fn for the class.
         fn = #TODO
-        # Here we calculate precision for the class
+        # Here we calculate the precision for the class.
         precision = #TODO
-        # Here we calculate recall for the class
+        # Here we calculate the recall for the class.
         recall =#TODO
-        # Here we add a tuple containing a class, its precision and recall to the resulting array
+        # Here we add a tuple containing the class and its precision and recall to the resulting array.
         class_precision_recall.append((c, precision, recall))
-    # The returned value would be an array of tuples containing unique classes and precision/ recall
+    # The returned value would be an array of tuples containing the unique classes and the precision/recall
     # values corresponding to them, like this: [(class 1, precision 1, recall 1), ...
-    # ... , (class n, precision n, recall n)
+    # ... , (class n, precision n, recall n)].
     return class_precision_recall
 
 
