d916f017f0e0560c8c9f75416f082e860d89e85a
sofiiako <19rubisco93@gmail.com>
2021-05-08 17:48:43 +0300

    Added layer1 test to feedforward

--- Iris Network/Forward Step/tests/test_task.py
+++ Iris Network/Forward Step/tests/test_task.py
@@ -14,5 +14,16 @@ class TestCase(unittest.TestCase):
         y_train = np.array([[0, 1, 1, 0]]).T
         nn = NN(3, 3, 1)
         y_predicted = nn.feedforward(X_train)
-        self.assertEqual(4, y_predicted.shape[0])
-        self.assertEqual(1, y_predicted.shape[1])
+        self.assertSequenceEqual((4, 1), y_predicted.shape)
+        #self.assertEqual(4, y_predicted.shape[0])
+        #self.assertEqual(1, y_predicted.shape[1])
+    def test_layer_shape(self):
+        X_train = np.array([[0, 0, 1],
+                            [1, 1, 1],
+                            [1, 0, 1],
+                            [0, 1, 1]])
+
+        y_train = np.array([[0, 1, 1, 0]]).T
+        nn = NN(3, 3, 1)
+        nn.feedforward(X_train)
+        self.assertSequenceEqual((4, 3), nn.layer1.shape)
