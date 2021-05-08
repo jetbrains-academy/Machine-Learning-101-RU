73fa953de5b6cced6a50beca337a72d6fd8f880b
sofiiako <19rubisco93@gmail.com>
2021-05-08 17:54:25 +0300

    Added msgs to test in feedforward

--- Iris Network/Forward Step/tests/test_task.py
+++ Iris Network/Forward Step/tests/test_task.py
@@ -14,7 +14,7 @@ class TestCase(unittest.TestCase):
         y_train = np.array([[0, 1, 1, 0]]).T
         nn = NN(3, 3, 1)
         y_predicted = nn.feedforward(X_train)
-        self.assertSequenceEqual((4, 1), y_predicted.shape)
+        self.assertTupleEqual((4, 1), y_predicted.shape, msg="Something wrong with the output!")
         #self.assertEqual(4, y_predicted.shape[0])
         #self.assertEqual(1, y_predicted.shape[1])
     def test_layer_shape(self):
@@ -26,4 +26,4 @@ class TestCase(unittest.TestCase):
         y_train = np.array([[0, 1, 1, 0]]).T
         nn = NN(3, 3, 1)
         nn.feedforward(X_train)
-        self.assertSequenceEqual((4, 3), nn.layer1.shape)
+        self.assertTupleEqual((4, 3), nn.layer1.shape, msg="Something wrong with layer 1!")
