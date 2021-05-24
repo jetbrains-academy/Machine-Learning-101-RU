9525a9c205af59d5d3c78af28df740e21ae930b3
sofiiako <19rubisco93@gmail.com>
2021-05-24 17:10:29 +0300

    Added Bayes lesson

---
+++ Bayes Guards SMS/Vectorize/task.py
@@ -0,0 +1,25 @@
+import numpy as np
+import string
+
+
+def split_by_words(X):
+    return np.core.chararray.lower(X).translate(str.maketrans('', '', string.punctuation)).split()
+
+
+def vectorize(X):
+    X_len = len(X)
+    X = split_by_words(X)
+
+    uniques = np.unique(np.hstack(X))
+    index_dict = {}
+    for index, word in enumerate(uniques):
+        index_dict[word] = index
+
+    vectorization = np.zeros((X_len, len(index_dict)), dtype=np.int64)
+    for index, message in enumerate(X):
+        unique, count = np.unique(message, return_counts=True)
+        for i, word in enumerate(unique):
+            word_index = index_dict[word]
+            vectorization[index, word_index] = count[i]
+
+    return index_dict, vectorization
---
+++ Bayes Guards SMS/Vectorize/tests/test_task.py
@@ -0,0 +1,21 @@
+import unittest
+import numpy as np
+from numpy.ma.testutils import assert_array_equal
+
+from task import vectorize
+
+
+class TestCase(unittest.TestCase):
+    def test_dictionary(self):
+        X = np.array(['Who let the dogs out?', 'Who, who, who, who?'])
+        dictionary, result = vectorize(X)
+        keys = {'dogs', 'let', 'out', 'the', 'who'}
+        values = {0, 1, 2, 3, 4}
+        self.assertEqual(keys, set(dictionary.keys()))
+        self.assertEqual(values, set(dictionary.values()))
+
+    def test_vectorization(self):
+        X = np.array(["Who let the dogs out?", " Who, who, who, who?"])
+        dictionary, result = vectorize(X)
+        assert_array_equal(np.array([1, 1, 1, 1, 1]), result[0])
+        self.assertTrue(4 in result[1])
