78260bc8ff6a8b4b91c34628faec65e5a048dc4e
sofiiako <19rubisco93@gmail.com>
2021-06-08 17:03:15 +0300

    Added tests to check if the correct activation func is employed;

--- Iris Network/Neurons/tests/test_task.py
+++ Iris Network/Neurons/tests/test_task.py
@@ -9,3 +9,12 @@ class TestCase(unittest.TestCase):
         self.assertEqual(0.27, round(sigmoid(-1), 2))
         self.assertEqual(0.73, round(sigmoid(1), 2))
         self.assertEqual(0.95, round(sigmoid(3), 2))
+
+    def test_relu(self):
+        self.assertNotEqual(0, sigmoid(-1), msg="You should use sigmoid activation function, not ReLU!")
+        self.assertNotEqual(1, sigmoid(1), msg="You should use sigmoid activation function, not ReLU!")
+        self.assertNotEqual(100, sigmoid(100), msg="You should use sigmoid activation function, not ReLU!")
+
+    def test_tanh(self):
+        self.assertNotEqual(0, sigmoid(0), msg="You should use sigmoid activation function!")
+        self.assertNotEqual(0.9951, round(sigmoid(3), 4), msg="You should use sigmoid activation function, not tanh!")
