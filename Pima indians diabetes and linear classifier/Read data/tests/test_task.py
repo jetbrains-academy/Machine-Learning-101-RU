9af977de0ca2e7566a329f0b15f99b94e79548dd
Igor <igor.gerasimov@jetbrains.com>
2021-12-08 14:02:28 +0300

    fix(Pima Indians - Read Data): test_task.py .csv paths fixed fixed
    Closes EDC-385

--- Pima indians diabetes and linear classifier/Read data/tests/test_task.py
+++ Pima indians diabetes and linear classifier/Read data/tests/test_task.py
@@ -12,13 +12,13 @@ from task import read_data
 
 class TestCase(unittest.TestCase):
     def test_X(self):
-        X, y = read_data("./task/pima-indians-diabetes.csv")
+        X, y = read_data("pima-indians-diabetes.csv")
         self.assertEquals((768, 9), X.shape, "Wrong train data length")
 
     def test_y(self):
-        X, y = read_data("./task/pima-indians-diabetes.csv")
+        X, y = read_data("pima-indians-diabetes.csv")
         self.assertEquals(768, len(y), "Wrong train data length")
 
     def test_y_value(self):
-        X, y = read_data("./task/pima-indians-diabetes.csv")
+        X, y = read_data("pima-indians-diabetes.csv")
         self.assertTrue(((y == -1) | (y == 1)).all(), "y array should contain only -1 and 1 values")
